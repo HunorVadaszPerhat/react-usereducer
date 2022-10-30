52db6120550a95c3da08e3d245c9ccbd1cb0dc78
Hunor Vadasz-Perhat <vadaszperhat.hunor@gmail.com>
2022-10-30 19:35:07 +0100

    added Form reducer

--- usereducer-app/src/App.js
+++ usereducer-app/src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import React from 'react';
 // import Post from './components/usestate/post/Post';
-import Post from './components/usereducer/post/Post';
+// import Post from './components/usereducer/post/Post';
+import Form from './components/usestate/forms/Form';
+import FormReducer from './components/usereducer/post/FormReducer';
 
 function App() {
   return (
       <React.Fragment> 
         {/* <Post/> */}
-        <Post/>
+        {/* <Post/> */}
+        {/* <Form/> */}
+        <FormReducer/>
       </React.Fragment>
   );
 }
